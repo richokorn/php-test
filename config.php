2844b95f1b3521f41b9ba4d0175bcff1cc4528b4
Richard Korn <richo.korn@gmail.com>
2022-06-09 14:02:11 +0200

    Create register.php, Download XAMPP

--- config.php
+++ config.php
@@ -1,16 +1,7 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-$connection_string = 'localhost', 'php_test', 'php_test', 'php_test';
 
-// define('DB_SERVER', 'localhost');
-// define('DB_USERNAME', 'root');
-// define('DB_PASSWORD', '');
-// define('DB_NAME', 'demo');
-
-/* Attempt to connect to MySQL database */
-// $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-$link = pg_connect(string $connection_string, int $flags = 0): PgSql\Connection|false
+// Attempt to connect to PostgreSQL database
+$link = pg_connect("host=localhost port=5432 dbname=php_test user=php_test password=php_test");
 
 // Check connection
 if($link === false){
